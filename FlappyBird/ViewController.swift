eee78bfad6afbd2fe3852d7e1e4fbabf7f83c48a
taku.takamura <taku.takamura@tecahademy.jp>
2019-11-01 23:41:41 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by 高村拓 on 2019/11/01.
+//  Copyright © 2019 taku.takamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
